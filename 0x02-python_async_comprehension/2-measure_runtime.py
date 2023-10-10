f50cefbc1c3fd54d4ad266b04a43817e553bd0ab
OChibu <beze4luv@yahoo.com>
2023-10-10 14:21:50 -0700

    Python - Async Comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,24 @@
-
 #!/usr/bin/env python3
-""" Async Comprehensions """
-from asyncio import gather
-from time import time
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+"""
+This module provides an asynchronous function that measures the runtime
+of running 4 instances of the async_comprehension function from the
+'1-async_comprehension' module
+"""
+import asyncio
+import time
+from importlib import import_module as using
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
 async def measure_runtime() -> float:
-    """ Run time for four parallel comprehensions """
-    start = time()
-    tasks = [async_comprehension() for i in range(4)]
-    await gather(*tasks)
-    end = time()
-    return (end - start)
+    """
+    Asynchronous function that measures and returns the runtime of
+    running 4 instances of the async_comprehension function.
+
+    Returns:
+        float: The runtime of the async_comprehension function in seconds.
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
